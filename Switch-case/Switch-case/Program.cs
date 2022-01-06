36be1305e64569ac0e58be411824d2d411e59a49
sonereraslan <sonereraslan@gmail.com>
2022-01-06 17:06:15 +0300

    Switch-case

---
+++ Switch-case/Switch-case/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındayız!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındayız!");
+                    break;
+                    
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındayız!");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındayız!");
+                    break;
+
+
+
+            }
+        }
+    }
+}
