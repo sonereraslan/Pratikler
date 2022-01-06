821a7763c48fb18b472fcbf67661b1ecf8de1e75
sonereraslan <sonereraslan@gmail.com>
2022-01-06 22:44:10 +0300

    While-Loop

---
+++ While-foreach/While-foreach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı girini:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("1'den girilen sayıya kadar olan sayıların ortalaması: "+toplam / sayi);
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
