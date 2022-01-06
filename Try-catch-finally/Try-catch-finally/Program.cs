2a1e888b5cfc990442349e45e03b38a9cd2dc789
sonereraslan <sonereraslan@gmail.com>
2022-01-06 16:33:29 +0300

    Try catch finally

---
+++ Try-catch-finally/Try-catch-finally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("a değerini girin:");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("b değerini girin:");
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine("a+b= "+c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
