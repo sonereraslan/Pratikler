051dcdb286e5e0538f5b2c8228cb3a3290e13cd7
sonereraslan <sonereraslan@gmail.com>
2022-01-06 16:21:45 +0300

    Conversions

---
+++ Conversions/Conversions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Implicit Conversion****");
+            int a = 10;
+            double b = a;
+            int c = (int)b;
+            Console.WriteLine("b: "+b);
+            Console.WriteLine("c: "+c);
+
+            Console.WriteLine("**** Explicit Conversion****");
+
+            float x = 5.7f;
+            byte y = (byte)x;
+            Console.WriteLine("y: " + y);
+
+            int xx = 5;
+            string yy = xx.ToString();
+            Console.WriteLine("yy: " + yy);
+
+        }
+    }
+}
