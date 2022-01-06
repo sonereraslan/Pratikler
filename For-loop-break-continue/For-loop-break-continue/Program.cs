58801ea830efad8cb52fe025539d1b4f5d5174b6
sonereraslan <sonereraslan@gmail.com>
2022-01-06 22:21:33 +0300

    For loop break continue

---
+++ For-loop-break-continue/For-loop-break-continue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace For_loop_break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tekToplam = 0;
+            int ciftToplam = 0;
+
+            for(int i=1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i;
+                else
+                    ciftToplam += i;
+            }
+
+            Console.WriteLine("Tek toplam: " + tekToplam);
+            Console.WriteLine("Çift toplam: " + ciftToplam);
+        }
+    }
+}
